7734e7330d5d7bd186205ba3759a81ff2301e759
Frozenki <frozenki4964@gmail.com>
2024-02-24 22:51:56 +0800

    测试

---
+++ while.c
@@ -0,0 +1,31 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.8.34511.84
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "while", "while\while.vcxproj", "{B3AE2E25-2C91-4480-A3B9-77AC138030D8}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|x64 = Debug|x64
+		Debug|x86 = Debug|x86
+		Release|x64 = Release|x64
+		Release|x86 = Release|x86
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{B3AE2E25-2C91-4480-A3B9-77AC138030D8}.Debug|x64.ActiveCfg = Debug|x64
+		{B3AE2E25-2C91-4480-A3B9-77AC138030D8}.Debug|x64.Build.0 = Debug|x64
+		{B3AE2E25-2C91-4480-A3B9-77AC138030D8}.Debug|x86.ActiveCfg = Debug|Win32
+		{B3AE2E25-2C91-4480-A3B9-77AC138030D8}.Debug|x86.Build.0 = Debug|Win32
+		{B3AE2E25-2C91-4480-A3B9-77AC138030D8}.Release|x64.ActiveCfg = Release|x64
+		{B3AE2E25-2C91-4480-A3B9-77AC138030D8}.Release|x64.Build.0 = Release|x64
+		{B3AE2E25-2C91-4480-A3B9-77AC138030D8}.Release|x86.ActiveCfg = Release|Win32
+		{B3AE2E25-2C91-4480-A3B9-77AC138030D8}.Release|x86.Build.0 = Release|Win32
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {1938C16C-92E9-42F2-BFAD-3B8E2DA9C99A}
+	EndGlobalSection
+EndGlobal
